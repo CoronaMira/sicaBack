36d159347d30447208399f91094514bbed48b2a4
Nayeli Corona <corona.mira.nayeli@gmail.com>
2025-07-30 11:43:35 -0600

    fingerprint migration

---
+++ src/main/resources/db/migration/V5__create_fingerprint_table.sql
@@ -0,0 +1,20 @@
+CREATE TABLE fingerprints
+(
+    id                BIGINT AUTO_INCREMENT PRIMARY KEY,
+    person_id         BIGINT       NULL,
+    student_id        INT          NULL,
+    fingerprint_data  LONGBLOB     NOT NULL,
+    finger            VARCHAR(50)  NOT NULL,
+    active            TINYINT(1) DEFAULT 1,
+    registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE attendance_records
+(
+    id               BIGINT AUTO_INCREMENT PRIMARY KEY,
+    person_id        BIGINT       NOT NULL,
+    record_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    record_type      VARCHAR(20)  NOT NULL,
+    device_id        VARCHAR(100) NULL,
+    CONSTRAINT fk_attendance_person FOREIGN KEY (person_id) REFERENCES person (id)
+);
