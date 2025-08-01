e94c39f2b565308d2cc63a45f71ae104bb81d3ce
Nayeli Corona <corona.mira.nayeli@gmail.com>
2025-08-01 02:43:06 -0600

    add persons

--- src/main/resources/db/migration/V6__insert_initial_records.sql
+++ src/main/resources/db/migration/V6__insert_initial_records.sql
@@ -1,3 +1,8 @@
+INSERT INTO escuela.person (id, first_name, last_name, middle_name, type, enrollment, degree, shift) VALUES (1, 'Hector', 'Corona', 'Osorio', 'student', '234536664', 'Desarrollo de Software', 'Vespertino');
+INSERT INTO escuela.person (id, first_name, last_name, middle_name, type, enrollment, degree, shift) VALUES (2, 'Viridiana', 'Hernandez', 'Baez', 'student', '24223198', 'Desarrollo de Software', 'Vespertino');
+INSERT INTO escuela.person (id, first_name, last_name, middle_name, type, enrollment, degree, shift) VALUES (3, 'Carlos', 'Martinez', 'Baez', 'student', '2322455', 'Desarrollo de Software', 'Matutino');
+INSERT INTO escuela.person (id, first_name, last_name, middle_name, type, enrollment, degree, shift) VALUES (4, 'Nayeli', 'Martinez', 'Diaz', 'student', '2453677809', null, 'Matutino');
+
 
 INSERT INTO attendance_records (person_id, record_timestamp, record_type, device_id, gate, status)
 VALUES (1, '2025-07-21 09:02:15', 'ENTRY', 'DEV001', 'MAIN_GATE', 'SUCCESS');
