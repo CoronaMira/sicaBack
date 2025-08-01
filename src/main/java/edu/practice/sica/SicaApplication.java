8faf3a34b638463c84d37d7da7d12c5411319b8c
Nayeli Corona <corona.mira.nayeli@gmail.com>
2025-08-01 09:52:38 -0600

    Cambios

--- src/main/java/edu/practice/sica/SicaApplication.java
+++ src/main/java/edu/practice/sica/SicaApplication.java
@@ -1,12 +1,8 @@
 package edu.practice.sica;
-
-import com.zkteco.biometric.SicaEnrollmentTool;
-import edu.practice.sica.service.FingerprintService;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import javax.swing.SwingUtilities;
 
 
 @SpringBootApplication
@@ -15,11 +11,6 @@ public class    SicaApplication {
         ConfigurableApplicationContext context = new SpringApplicationBuilder(SicaApplication.class)
                 .headless(false)
                 .run(args);
-        FingerprintService fingerprintService = context.getBean(FingerprintService.class);
-
-        SwingUtilities.invokeLater(() -> {
-            new SicaEnrollmentTool(fingerprintService);
-        });
     }
 }
 
